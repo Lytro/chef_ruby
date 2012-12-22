21e4275a7df57b41bbbe041d4e6eda29d34193bd
Anuj Biyani <abiyani@lytro.com>
2012-12-21 16:05:11 -0800

    write spec for installing necessary packages

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,10 +1,18 @@
 require 'chefspec'
 
 describe 'chef-ruby1.9::default' do
-  let(:chef_run) { ChefSpec::ChefRunner.new.converge 'chef-ruby1.9::default' }
+  let(:runner) { ChefSpec::ChefRunner.new }
+  let(:chef_run) { runner.converge 'chef-ruby1.9::default' }
 
-  it "pending" do
-    pending "implement me!"
-    chef_run.should do_something
+  before do
+    Chef::Recipe.any_instance.stub(:load_recipe).and_return do |arg|
+      runner.node.run_state[:seen_recipes][arg] = true
+    end
+  end
+
+  %w( wget zlib1g-dev libssl-dev libffi-dev libxml2-dev libncurses-dev libreadline-dev libyaml-0-2 libyaml-dev ).each do |package|
+    it "installs package #{package}" do
+      chef_run.should install_package package
+    end
   end
 end
