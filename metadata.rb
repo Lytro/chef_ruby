74e8d73bffe5b3883abb2b83be7acfc7398fe66a
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-09-03 21:51:58 +0100

    Installs Ruby 1.9 or 2.0

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              'chef_ruby'
 maintainer        "Lytro"
 maintainer_email  "cookbooks@lytro.com"
 license           "WTFPL"
-description       "Installs Ruby 1.9 from source"
+description       "Installs Ruby from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
 version           "2.2.2"
 supports          "ubuntu"
@@ -11,4 +11,4 @@ supports          "ubuntu"
   depends d
 end
 
-recipe            "chef_ruby::default", "Installs Ruby 1.9 from source."
+recipe            "chef_ruby::default", "Installs Ruby from source."
