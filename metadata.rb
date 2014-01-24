e805c6f9f00c6c9592ac08ff331bfa415d0e740e
Anuj Biyani <abiyani@lytro.com>
2014-01-24 11:29:57 -0800

    bump to version 2.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@lytro.com"
 license           "WTFPL"
 description       "Installs Ruby from source"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
-version           "2.2.1"
+version           "2.3.0"
 supports          "ubuntu"
 
 %w( apt build-essential ).each do |d|
